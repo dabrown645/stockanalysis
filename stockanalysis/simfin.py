58a1335e9df9e568939fc99792e2254a6de9df95
David A Brown <dabrown645@gmail.com>
2019-04-25 22:45:17 -0700

    Add more tests and fixed get_statement

--- stockanalysis/simfin.py
+++ stockanalysis/simfin.py
@@ -15,16 +15,27 @@ avail_statements = "statements/list"
 def get_url(url):
     try:
         content = requests.get(url, verify=False)
-    except requests.exceptions as e:
-        print(f'exception: {e}')
-
-    return content.json()
+        content.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        if content.status_code == 200:
+            return content.json()
+        else:
+            print(f'Status code: {content.status_code}')
+            return None
 
 
 def get_simfin_ids(ticker):
     url = f'{simfin_url}{id_query}{ticker}?{api_key}'
     simfin_ids = get_url(url)
-    return simfin_ids
+    if  simfin_ids:
+        return simfin_ids
+    else:
+        print(f'ticker {ticker} NOT found')
+        return None
 
 
 def get_company_info(ticker):
@@ -45,11 +56,11 @@ def get_available_statements(simfin_id):
 def get_statement(simfin_id, stype, year):
     statement = {}
     url = f'{simfin_url}{company_query}{simfin_id}/{statement_query}?stype={stype}&fyear={year}&ptype=FY&{api_key}'
-    statement_data = get_urt(url)
+    statement_data = get_url(url)
     row_num=0
     if len(statement) == 0:
         statement[row_num] = ["Description", year]
-    for value in statement_data["values]"]:
+    for value in statement_data["values"]:
         row_num += 1
         if value["valueChosen"] is None:
             value_chosen = None
@@ -63,8 +74,15 @@ if __name__ == "__main__":
     ticker = ticker.upper()
 
     simfin_ids = get_simfin_ids(ticker)
-    print(f'simfin_ids: {simfin_ids}')
+    if simfin_ids == None:
+        exit()
+    else:
+        print(f'simfin_ids: {simfin_ids}')
     company_info = get_company_info(ticker)
     print(f'company_info: {company_info}')
     for simfin_id in simfin_ids:
         print(f'available_statements: {get_available_statements(simfin_id["simId"])}')
+        available_statements = get_available_statements(simfin_id["simId"])
+        print(f'available_statements keys: {available_statements.keys()}')
+        for stype in available_statements.keys():
+            print(f'statement type: {stype} details {get_statement(simfin_id["simId"], stype, 2017)}')
